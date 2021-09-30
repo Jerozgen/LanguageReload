0d870b3c28dcc01d460d2525bbbc030dbee538bd
Jerozgen <jerozgen@gmail.com>
2021-09-30 05:54:54 +0300

    Allow the use of F3 + J everywhere

--- src/main/java/jerozgen/languagereload/mixin/MixinKeyboard.java
+++ src/main/java/jerozgen/languagereload/mixin/MixinKeyboard.java
@@ -2,12 +2,15 @@ package jerozgen.languagereload.mixin;
 
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.TranslatableText;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Keyboard.class)
@@ -19,17 +22,9 @@ public abstract class MixinKeyboard {
     @Shadow
     protected abstract void debugLog(String key, Object... args);
 
-    @Inject(
-            method = "processF3(I)Z",
-            at = @At("TAIL"),
-            cancellable = true
-    )
-    private void onProcessF3$addKey(int key, CallbackInfoReturnable<Boolean> cir) {
-        if (key == 'J') {
-            client.getLanguageManager().reload(client.getResourceManager());
-            this.debugLog("debug.reload_languages.message");
-            cir.setReturnValue(true);
-        }
+    private void reloadLanguages() {
+        client.getLanguageManager().reload(client.getResourceManager());
+        this.debugLog("debug.reload_languages.message");
     }
 
     @Inject(
@@ -43,5 +38,47 @@ public abstract class MixinKeyboard {
     )
     private void onProcessF3$addHelp(int key, CallbackInfoReturnable<Boolean> cir) {
         this.client.inGameHud.getChatHud().addMessage(new TranslatableText("debug.reload_languages.help"));
+    }
+
+    @Inject(
+            method = "processF3(I)Z",
+            at = @At("TAIL"),
+            cancellable = true
+    )
+    private void onProcessF3(int key, CallbackInfoReturnable<Boolean> cir) {
+        if (key == GLFW.GLFW_KEY_J) {
+            reloadLanguages();
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(
+            method = "onKey(JIIII)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/screen/Screen;wrapScreenError(Ljava/lang/Runnable;Ljava/lang/String;Ljava/lang/String;)V"
+            ),
+            cancellable = true
+    )
+    private void onOnKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        if (InputUtil.isKeyPressed(window, GLFW.GLFW_KEY_F3) && key == GLFW.GLFW_KEY_J) {
+            if (action != 0) reloadLanguages();
+            ci.cancel();
+        }
+    }
+
+    @Inject(
+            method = "onChar(JII)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/screen/Screen;wrapScreenError(Ljava/lang/Runnable;Ljava/lang/String;Ljava/lang/String;)V",
+                    ordinal = 0
+            ),
+            cancellable = true
+    )
+    private void onOnChar(long window, int codePoint, int modifiers, CallbackInfo ci) {
+        if (InputUtil.isKeyPressed(window, GLFW.GLFW_KEY_F3) && InputUtil.isKeyPressed(window, GLFW.GLFW_KEY_J)) {
+            ci.cancel();
+        }
     }
 }
