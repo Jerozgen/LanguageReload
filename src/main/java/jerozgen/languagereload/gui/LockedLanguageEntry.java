c21588759598d1537263b20cd8af4248302856e8
Jerozgen <jerozgen@gmail.com>
2022-12-06 19:23:18 +0300

    1.19.3

--- src/main/java/jerozgen/languagereload/gui/LockedLanguageEntry.java
+++ src/main/java/jerozgen/languagereload/gui/LockedLanguageEntry.java
@@ -1,36 +1,23 @@
 package jerozgen.languagereload.gui;
 
+import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TexturedButtonWidget;
 import net.minecraft.client.resource.language.LanguageDefinition;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 
 import java.util.LinkedList;
-import java.util.function.Consumer;
 
 public class LockedLanguageEntry extends LanguageEntry {
     private final Text LOCK_BUTTON_TOOLTIP = Text.translatable("language.default.tooltip");
 
     private final ButtonWidget lockButton = addChild(new TexturedButtonWidget(0, 0, 16, 24, 43, 0, 0,
-            TEXTURE, TEXTURE_WIDTH, TEXTURE_HEIGHT, __ -> {}, new ButtonWidget.TooltipSupplier() {
-        @Override
-        public void onTooltip(ButtonWidget button, MatrixStack matrices, int mouseX, int mouseY) {
-            if (client.currentScreen != null) {
-                client.currentScreen.renderTooltip(matrices, LOCK_BUTTON_TOOLTIP, mouseX, mouseY);
-            }
-        }
-
-        @Override
-        public void supply(Consumer<Text> consumer) {
-            consumer.accept(LOCK_BUTTON_TOOLTIP);
-        }
-    }, ScreenTexts.EMPTY));
+            TEXTURE, TEXTURE_WIDTH, TEXTURE_HEIGHT, __ -> {}));
 
     public LockedLanguageEntry(Runnable refreshListsAction, LanguageDefinition language, LinkedList<LanguageDefinition> selectedLanguages) {
         super(refreshListsAction, language, selectedLanguages);
         lockButton.active = false;
+        lockButton.setTooltip(Tooltip.of(LOCK_BUTTON_TOOLTIP));
     }
 
     @Override
--- src/main/java/jerozgen/languagereload/mixin/LanguageOptionsScreenMixin.java
+++ src/main/java/jerozgen/languagereload/mixin/LanguageOptionsScreenMixin.java
@@ -79,7 +79,9 @@ public abstract class LanguageOptionsScreenMixin extends GameOptionsScreen {
         refresh();
 
         addDrawableChild(gameOptions.getForceUnicodeFont().createButton(gameOptions, width / 2 - 155, height - 28, 150));
-        addDrawableChild(new ButtonWidget(width / 2 - 155 + 160, height - 28, 150, 20, ScreenTexts.DONE, this::onDone));
+        addDrawableChild(ButtonWidget.builder(ScreenTexts.DONE, this::onDone)
+                .dimensions(width / 2 - 155 + 160, height - 28, 150, 20)
+                .build());
 
         super.init();
         ci.cancel();
