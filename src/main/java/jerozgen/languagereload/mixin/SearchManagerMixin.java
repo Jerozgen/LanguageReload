9bc08b4ccb1e4e07741a3236d8ffbf14e0a91d7a
Jerozgen <jerozgen@gmail.com>
2024-10-24 07:39:23 +0300

    Fix multilingual search doesn't work with Figura

--- src/main/java/jerozgen/languagereload/mixin/SearchManagerMixin.java
+++ src/main/java/jerozgen/languagereload/mixin/SearchManagerMixin.java
@@ -1,32 +1,48 @@
 package jerozgen.languagereload.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import jerozgen.languagereload.access.ILanguage;
 import jerozgen.languagereload.access.ITranslationStorage;
 import jerozgen.languagereload.config.Config;
 import net.minecraft.client.search.SearchManager;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.text.Text;
 import net.minecraft.util.Language;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mixin(value = SearchManager.class, priority = 990)
 abstract class SearchManagerMixin {
-    @ModifyExpressionValue(method = "method_60363", at = @At(value = "INVOKE", target = "Lnet/minecraft/text/Text;getString()Ljava/lang/String;"))
-    private static String addFallbackTranslationsToSearchTooltips(String original, Text tooltip) {
+    @WrapOperation(method = {"method_60365"},
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;getTooltip(Lnet/minecraft/item/Item$TooltipContext;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/tooltip/TooltipType;)Ljava/util/List;"))
+    private static List<Text> addFallbackTranslationsToSearchTooltips(ItemStack instance, Item.TooltipContext context, @Nullable PlayerEntity player, TooltipType type, Operation<List<Text>> operation) {
+        var original = operation.call(instance, context, player, type);
+
         if (Config.getInstance() == null) return original;
         if (!Config.getInstance().multilingualItemSearch) return original;
 
         var translationStorage = ((ILanguage) Language.getInstance()).languagereload_getTranslationStorage();
         if (translationStorage == null) return original;
 
-        var stringBuilder = new StringBuilder(original);
-        for (String fallbackCode : Config.getInstance().fallbacks) {
+        var result = new ArrayList<>(original);
+        for (var fallbackCode : Config.getInstance().fallbacks) {
             ((ITranslationStorage) translationStorage).languagereload_setTargetLanguage(fallbackCode);
-            stringBuilder.append('\n').append(tooltip.getString());
+            operation.call(instance, context, player, type)
+                    .stream()
+                    .map(Text::getString)
+                    .map(Text::literal)
+                    .forEach(result::add);
         }
 
         ((ITranslationStorage) translationStorage).languagereload_setTargetLanguage(null);
-        return stringBuilder.toString();
+        return result;
     }
 }
